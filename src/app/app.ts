6b4260b62b189ef60026b52f7d36a3aa430d6418
alison <adevpr11@gmail.com>
2026-02-11 11:42:18 -0300

    feat: adiciona interface de cadastro

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected title = 'web-clientes';
+  private httpClient = inject(HttpClient);
+
+  mensagemErro = signal<string>('');
+  mensagemSucesso = signal<string>('');
+
+  formulario = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    telefone: new FormControl('', [Validators.required, Validators.minLength(11),
+    ]),
+  });
+
+  cadastrar() {
+    
+    if (this.formulario.invalid) {
+      return;
+    }
+
+    const novoCliente = this.formulario.getRawValue();
+
+    this.httpClient
+      .post('http://localhost:8081/api/v1/clientes', novoCliente, {responseType: 'text'})
+      .subscribe(
+        {
+        next: (data: any) => {
+          this.mensagemSucesso.set(data);
+          this.formulario.reset();
+        },
+        error: (err:any) => {
+          console.log(err);
+          this.mensagemErro.set(err.error);
+        },
+      });
+  }
 }
