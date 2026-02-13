10d66189dd51663d84e7c88b79eab787436f044c
alison <adevpr11@gmail.com>
2026-02-13 12:11:02 -0300

    feat: implementa operações de exclusão e atualização

--- src/app/app.ts
+++ src/app/app.ts
@@ -22,6 +22,8 @@ export class App {
   mensagemErro = signal<string>('');
   mensagemSucesso = signal<string>('');
   listaClientes = signal<any[]>([]);
+  clienteSelecionadoParaExclusao = signal<any>(null);
+  clienteSelecionadoParaAtualizacao = signal<any>(null);
 
   apiUrl = 'http://localhost:8081/api/v1/clientes';
 
@@ -32,9 +34,26 @@ export class App {
     ]),
   });
 
+  formEdicao = new FormGroup({
+    id: new FormControl(''),
+    nome: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    telefone: new FormControl('', [Validators.required, Validators.minLength(11),
+    ]),
+  });
+
   formConsulta = new FormGroup({
   nome: new FormControl('', [Validators.required, Validators.minLength(6)])
 });
+
+listarTodos() {
+  this.httpClient.get<any[]>(this.apiUrl).subscribe({
+    next: (data) => {
+      this.listaClientes.set(data);
+    },
+    error: (err) => console.error('Erro ao listar todos', err)
+  });
+}
 
   cadastrar() {
 
@@ -63,16 +82,63 @@ export class App {
 
     const nome = this.formConsulta.controls.nome.value;
 
+    if(!nome){
+      this.listarTodos();
+      return;
+    }
+
     this.httpClient
       .get(`${this.apiUrl}/${nome}`)
       .subscribe(
         {
         next: (data) => {
           this.listaClientes.set(data as any);
-          this.formConsulta.reset();
+          // this.formConsulta.reset();
         },
         error: (err:any) => {
           console.log(err);
+          this.mensagemErro.set(err.error);
+        },
+      });
+  }
+
+  excluir() {
+
+  const id = this.clienteSelecionadoParaExclusao().id;
+
+    this.httpClient
+      .delete(`${this.apiUrl}/${id}`, {responseType: 'text'})
+      .subscribe(
+        {
+        next:(response) => {
+          this.mensagemSucesso.set(response);
+          this.consultar();
+        },
+        error: (err:any) => {
+          this.mensagemErro.set(err.error);
+        },
+      });
+  }
+
+  prepararEdicao(cliente:any){
+    this.clienteSelecionadoParaAtualizacao.set(cliente);
+    this.formEdicao.patchValue(cliente);
+  }
+
+  atualizar(){
+
+    const dadosClienteAtualizado = this.formEdicao.getRawValue();
+    const id = dadosClienteAtualizado.id;
+
+    this.httpClient
+      .put(`${this.apiUrl}/${id}`,dadosClienteAtualizado, {responseType: 'text'})
+      .subscribe(
+        {
+        next:(response) => {
+          this.mensagemSucesso.set(response);
+          this.consultar();
+        },
+        error: (err:any) => {
           this.mensagemErro.set(err.error);
         },
       });
